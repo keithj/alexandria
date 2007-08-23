78ed633a163a0becca115febf8c8c00f721026ec
Luis Oliveira <loliveira@common-lisp.net>
2007-08-23 07:05:00 +0300

    Small fix to REQUIRED-ARGUMENT's control string.

--- conditions.lisp
+++ conditions.lisp
@@ -4,7 +4,7 @@
   "Signals an error for a missing argument of NAME. Intended for
 use as an initialization form for structure and class-slots, and
 a default value for required keyword arguments."
-  (error "Required argument~@[~S ~] missing." name))
+  (error "Required argument ~@[~S ~]missing." name))
 
 (define-condition simple-style-warning (style-warning simple-warning)
   ())

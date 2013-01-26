107eb17cefb15e9a66b1301b970d5ca3aefc42d3
Nikodemus Siivola <nikodemus@random-state.net>
2013-01-26 16:25:56 +0200

    fix docstring of IOTA

--- numbers.lisp
+++ numbers.lisp
@@ -52,7 +52,7 @@ and STEP. START defaults to 0 and STEP to 1.
 
 Examples:
 
-  (iota 4)                      => (0 1 2 3 4)
+  (iota 4)                      => (0 1 2 3)
   (iota 3 :start 1 :step 1.0)   => (1.0 2.0 3.0)
   (iota 3 :start -1 :step -1/2) => (-1 -3/2 -2)
 "
